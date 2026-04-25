c8a2b5cc490fe44c8e506ec23b4c349b1fdbda30
Fia233 <sofiali2023@hotmail.com>
2026-04-26 01:21:24 +0800

    sensor jam

---
+++ 03-sensor-jam/sketch.js
@@ -0,0 +1,74 @@
+let port; // Serial Communication port
+let connectBtn;
+
+let sensorVal;
+let circleSize = 50;
+let rectSize = circleSize/2;
+let targetSize = 50; // used for Option 2
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  port = createSerial(); // creates the Serial Port
+
+  // Connection helpers
+  connectBtn = createButton("Connect to Arduino");
+  connectBtn.position(20, 20);
+  connectBtn.mousePressed(connectBtnClick);
+}
+
+function draw() {
+  background(100);
+  ellipse(width / 2, height / 2, circleSize);
+  rect(width / 2, height / 2, rectSize, rectSize);
+
+  // Receive data from Arduino
+  if (port.opened()) {
+    sensorVal = port.readUntil("\n");
+    // Only log data that has information, not empty signals
+    if (sensorVal[0]) {
+      let val = float(sensorVal);
+      // Once you verify data is coming in,
+      // disable logging to improve performance
+      // console.log(sensorVal);
+      console.log(val);
+
+      // OPTION 1:
+      // Update circle's size with sensor's data directly
+      // Reduce delay() value in Ardiuno to get smoother changes
+
+      // use float() to convert from data from string to number
+      // circleSize = float(sensorVal);
+
+      // OPTION 2:
+      // Update circle's size using lerp() to smoothly change values
+      // This method even works with longer delay() values in Arduino
+
+      // targetSize = float(sensorVal);
+      // last value in lerp() controls speed of change
+      circleSize = lerp(circleSize, val, 0.1);
+      rectSize = lerp(rectSize, val/2, 0.1);
+      
+
+      let c = map(val, 0, 1023, 0, 255);
+      //  fill(c);
+       fill(c, 100, 255 - c); // used help from genai
+       stroke(255 - c, 100, c);
+       strokeWeight(random(3, 7));
+    }
+  }
+}
+
+// DO NOT REMOVE THIS FUNCTION
+function connectBtnClick(e) {
+  // If port is not already open, open on click,
+  // otherwise close the port
+  if (!port.opened()) {
+    port.open(9600); // opens port with Baud Rate of 9600
+    e.target.innerHTML = "Disconnect Arduino";
+    e.target.classList.add("connected");
+  } else {
+    port.close();
+    e.target.innerHTML = "Connect to Arduino";
+    e.target.classList.remove("connected");
+  }
+}
